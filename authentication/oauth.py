f01c085509835c38c627f10bfe0cd8e8a05492f3
anjdal713 <anjdal713@naver.com>
2023-08-09 00:56:45 +0900

    restored

---
+++ authentication/oauth.py
@@ -0,0 +1,13 @@
+# 로그인 만든대서? 아니면 안쓰고
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
