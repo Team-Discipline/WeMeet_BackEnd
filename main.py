7e5400ecd15364ca2bb124e5b1261399c17b6463
gka0903 <ham3252@naver.com>
2023-08-23 12:28:04 +0900

    commit err

--- main.py
+++ main.py
@@ -25,12 +25,14 @@ app.include_router(subwayStation_router.router)
 
 @app.get("/maps/{place_name}")
 async def get_point(query):
-    return {"place_name": Coordinate.place_name,
+    return {
+            "place_name": Coordinate.place_name,
             "axis_x": Coordinate.axis_x,
             "axis_y": Coordinate.axis_y,
             "address": Coordinate.address,
             "place_url": Coordinate.place_url,
-            "place_ID": Coordinate.place_ID}
+            "place_ID": Coordinate.place_ID
+            }
 
 
 if __name__ == '__main__':
